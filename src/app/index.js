5b5af2c847e1a4f3d9c678741181d14fdf699c4b
ma2saka <takashi.matsusaka@me.com>
2018-09-11 11:49:50 +0900

    app分離

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello React!</h1>
+        <p>this is a component.</p>
+      </div>
+    );
+  }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import "./index.css";
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1> Hello React!</h1>
-        <p> this is a component.</p>
-      </div>
-    );
-  }
-}
+import App from './app'
 
 render(<App />, document.getElementById("content"));
 
